d189334520d5b2b722bcfc85089f137dedab377c
ntthanh010176 <ntthanh010176@gmail.com>
2024-12-25 15:39:22 +0700

    Add project files.

---
+++ Chuong_01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chuong_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to OOP with C#");
+            Console.Read();
+        }
+    }
+}
